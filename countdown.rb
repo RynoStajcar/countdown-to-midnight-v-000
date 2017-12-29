ea9a62dd443ce2e30ebbf5dac3dbb6767f5d2785
Ryne Stajcar <rynostajcar@gmail.com>
2017-12-29 21:14:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  if number == 12
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_output(number)
+  until number == 0
+    number -= 1
+    return "#{number} SECOND(S)"
+  end
+  return "finished"
+end 
+
+def countdown_with_sleep
 end
